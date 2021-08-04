46bc9fd0f47c1efe890d7dc725b4d260ed67a820
Tracy <chenchiuhui@gmail.com>
2021-08-04 22:31:05 +0800

    Add TodoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,42 @@
 <template>
-  
+  <li>
+    <input type="checkbox" v-model="todo.isDone">
+    <label :class="[todo.isDone ? 'completed' : '']">{{ todo.text }}</label>
+    <button class="btn" @click="remove(index)">刪除</button>
+    <button class="btn">編輯</button>
+  </li>
 </template>
 
 <script>
 export default {
+  props: [ 'todo_prop', 'index' ],
+  data() {
+    return {
+      todo: this.todo_prop
+    }
+  },
+  methods: {
+    remove(index) {
+      this.$emit('remove', index)
+    }
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
+li {
+  font-size: 16px;
+  padding: 10px 20px;
+}
+
+.btn {
+  float: right;
+  margin-right: 6px;
+}
+
+.completed {
+  text-decoration: line-through;
+}
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,71 +2,100 @@
   <div class="wrap-todos">
     <h1 class="todo-heading">Todo List: Vue.js Example</h1>
     <div>
-      <input type="text" v-model="newTodo" placeholder="加入新工作" @keyup.enter="add" class="new-item">
+      <input
+        type="text"
+        v-model="newTodo"
+        placeholder="加入新工作"
+        @keyup.enter="add"
+        class="new-item"
+      />
     </div>
     <ul>
-      <li>
-        <input type="checkbox" id="ckbx1" v-model="todos[0].isCompleted">
-        <label for="ckbx1" :class="[todos[0].isCompleted ? 'completed' : '']">{{ todos[0].text }}</label>
+      <todo-item
+        v-for="(todo, index) in todoList"
+        :key="todo.id"
+        :todo_prop="todo"
+        :index="index"
+        @remove="remove"
+      ></todo-item>
+      <!-- <li>
+        <input type="checkbox" id="ckbx1" v-model="todos[0].isDone">
+        <label for="ckbx1" :class="[todos[0].isDone ? 'completed' : '']">{{ todos[0].text }}</label>
         <button class="btn">編輯</button>
         <button class="btn">刪除</button>
       </li>
       <li>
-        <input type="checkbox" id="ckbx2" v-model="todos[1].isCompleted">
+        <input type="checkbox" id="ckbx2" v-model="todos[1].isDone">
         <label for="ckbx2">{{ todos[1].text }}</label>
       </li>
       <li>
-        <input type="checkbox" id="ckbx3" v-model="todos[2].isCompleted">
+        <input type="checkbox" id="ckbx3" v-model="todos[2].isDone">
         <label for="ckbx3">{{ todos[2].text }}</label>
-      </li>
+      </li> -->
     </ul>
-    <p>{{ todos }}</p>
-    
+    <p>{{ todoList }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import TodoItem from '@/components/TodoItem.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     // HelloWorld,
+    TodoItem,
   },
   data() {
     return {
       newTodo: '',
-      todos: [
+      todoList: [
         {
-          id: 0,
+          id: 111111,
           text: '打掃房間',
-          isCompleted: false,
-          isEdit: false
+          isDone: false,
+          isEdit: false,
         },
         {
-          id: 1,
+          id: 222222,
           text: '超市買東西',
-          isCompleted: false,
-          isEdit: false
+          isDone: false,
+          isEdit: false,
         },
         {
-          id: 2,
+          id: 333333,
           text: '約小美喝咖啡',
-          isCompleted: false,
-          isEdit: false
+          isDone: false,
+          isEdit: false,
         },
       ],
-      finished: []
+      finished: [],
     }
-  }
-};
+  },
+  methods: {
+    add() {
+      console.log(+new Date())
+      this.todoList.push({
+        id: +new Date(),
+        text: this.newTodo,
+        isDone: false,
+        isEdit: false,
+      })
+      this.newTodo = ''
+    },
+    remove(index) {
+      this.todoList.splice(index, 1)
+    },
+  },
+}
 </script>
 
 <style scoped>
 .wrap-todos {
   width: 600px;
-  height: 500px;
+  height: 600px;
   background: rgb(205, 245, 209);
   border-radius: 5px;
   border: 1px solid #ccc;
@@ -83,20 +112,5 @@ ul {
   list-style: none;
   text-align: left;
   padding: 20px 100px;
-  outline: 1px red solid;
-}
-
-li {
-  font-size: 16px;
-  padding: 10px 20px;
-}
-
-.btn {
-  float: right;
-  margin-right: 6px;
-}
-
-.completed {
-  text-decoration: line-through;
 }
 </style>
